63db19dde132388566369c1ab2129e003b444d23
Zacklack <contact@zacklack.de>
2025-02-06 18:43:44 +0100

    Update TimelineSection: Revise content for 2021 and 2022, add new images and descriptions

--- src/components/TimelineSection.tsx
+++ src/components/TimelineSection.tsx
@@ -7,42 +7,16 @@ export default function TimelineSection() {
             title: "2023",
             content: (
                 <div>
-                    <p className="text-gray-300 text-xs md:text-sm font-normal mb-8">
-                        Launched my portfolio website showcasing projects like Website Scanner,
-                        NAS Management, and Discord Bot.
-                    </p>
+                    <div className="glass p-6 rounded-lg mb-8">
+                        <p className="text-gray-300 text-xs md:text-sm font-normal">
+                            Joined GitHub on 27th October 2023 and started looking into source control tools like git and Apache.
+                        </p>
+                    </div>
                     <div className="grid grid-cols-2 gap-4">
                         <div className="glass p-4 rounded-lg">
                             <img
-                                src="https://placeimg.com/500/500/tech"
-                                alt="Portfolio Project"
-                                width={500}
-                                height={500}
-                                className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full"
-                            />
-                        </div>
-                        <div className="glass p-4 rounded-lg">
-                            <img
-                                src="https://placeimg.com/500/500/tech/grayscale"
-                                alt="Portfolio Project"
-                                width={500}
-                                height={500}
-                                className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full"
-                            />
-                        </div>
-                        <div className="glass p-4 rounded-lg">
-                            <img
-                                src="https://placeimg.com/500/500/arch"
-                                alt="Portfolio Project"
-                                width={500}
-                                height={500}
-                                className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full"
-                            />
-                        </div>
-                        <div className="glass p-4 rounded-lg">
-                            <img
-                                src="https://placeimg.com/500/500/tech/sepia"
-                                alt="Portfolio Project"
+                                src="https://github.githubassets.com/assets/profile-joined-github-dark-6369d0efb8b9.svg"
+                                alt="Joined GitHub"
                                 width={500}
                                 height={500}
                                 className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full"
@@ -56,15 +30,16 @@ export default function TimelineSection() {
             title: "2022",
             content: (
                 <div>
-                    <p className="text-gray-300 text-xs md:text-sm font-normal mb-8">
-                        Explored new technologies and built multiple web apps using React,
-                        TypeScript, and Tailwind CSS.
-                    </p>
+                    <div className="glass p-6 rounded-lg mb-8">
+                        <p className="text-gray-300 text-xs md:text-sm font-normal">
+                            Bought my first VR Headset and started exploring Virtual Desktop. Aswell I bought professional audio equipment like a audio interface and a Beyerdynamic DT990 PRO. 
+                        </p>
+                    </div>
                     <div className="grid grid-cols-2 gap-4">
                         <div className="glass p-4 rounded-lg">
                             <img
-                                src="https://placeimg.com/500/500/nature"
-                                alt="Project Snapshot"
+                                src="https://m.media-amazon.com/images/I/51X81XdgPrL.jpg"
+                                alt="Pico 4 VR Headset"
                                 width={500}
                                 height={500}
                                 className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full"
@@ -72,8 +47,8 @@ export default function TimelineSection() {
                         </div>
                         <div className="glass p-4 rounded-lg">
                             <img
-                                src="https://placeimg.com/500/500/tech"
-                                alt="Project Snapshot"
+                                src="https://m-cdn.phonearena.com/images/article/152387-wide-two_1200/Virtual-Desktop-unveils-OpenXR-runtime-for-enhanced-VR-on-Quest-3.jpg"
+                                alt="Virtual Desktop"
                                 width={500}
                                 height={500}
                                 className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full"
@@ -92,6 +67,56 @@ export default function TimelineSection() {
                             <img
                                 src="https://placeimg.com/500/500/people"
                                 alt="Project Snapshot"
+                                width={500}
+                                height={500}
+                                className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full"
+                            />
+                        </div>
+                    </div>
+                </div>
+            ),
+        },
+        {
+            title: "2021",
+            content: (
+                <div>
+                    <div className="glass p-6 rounded-lg mb-8">
+                        <p className="text-gray-300 text-xs md:text-sm font-normal">
+                            Bought my first Synology NAS and started exploring NAS Management and Docker. Also I bought a new Car Multimedia System and learned about Can-Bus and OBD2 protocols. I also started exploring IoT and built a few projects around it.
+                        </p>
+                    </div>
+                    <div className="grid grid-cols-2 gap-4">
+                        <div className="glass p-4 rounded-lg">
+                            <img
+                                src="https://www.synology.com/img/products/detail/DS220plus/heading.png"
+                                alt="Synology DS 220+"
+                                width={500}
+                                height={500}
+                                className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full"
+                            />
+                        </div>
+                        <div className="glass p-4 rounded-lg">
+                            <img
+                                src="https://blog.iron.io/wp-content/uploads/2016/01/platform-docker-logo.png"
+                                alt="Docker Logo"
+                                width={500}
+                                height={500}
+                                className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full"
+                            />
+                        </div>
+                        <div className="glass p-4 rounded-lg">
+                            <img
+                                src="https://fleetgo.de/wp-content/uploads/CAN-Bus-1024x683.webp"
+                                alt="Can-Bus Protocol (Bosch)"
+                                width={500}
+                                height={500}
+                                className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full"
+                            />
+                        </div>
+                        <div className="glass p-4 rounded-lg">
+                            <img
+                                src="https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/325870478/original/074af8b9f3fd7a9043fe8d747192b0f35d03ea5b/do-iot-design-and-programming.jpg"
+                                alt="Internet of things"
                                 width={500}
                                 height={500}
                                 className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full"
